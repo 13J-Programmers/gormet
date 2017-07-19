43bd0f63f99734ccc21afff41ecedd85a3dda48e
nobuyo <world_fantasy_2017@live.jp>
2017-07-19 15:41:41 +0900

    migrate

--- db/migrate/20170719060351_create_orders.rb
+++ db/migrate/20170719060351_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
-      t.integer :status
+      t.integer :status, null: false
 
       t.timestamps
     end
--- db/migrate/20170719063914_create_menus.rb
+++ db/migrate/20170719063914_create_menus.rb
@@ -1,10 +1,10 @@
 class CreateMenus < ActiveRecord::Migration[5.1]
   def change
     create_table :menus do |t|
-      t.string :name
-      t.integer :amount
-      t.integer :stock
-      t.boolean :with_cheese
+      t.string :name, null: false
+      t.integer :amount, null: false
+      t.integer :stock, null: false
+      t.boolean :with_cheese, default: false
 
       t.timestamps
     end
