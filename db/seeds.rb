6506736fade2bbe5fa02de20bf5011a8b2de9567
nobuyo <world_fantasy_2017@live.jp>
2017-08-18 01:16:25 +0900

    modify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
+# orders
+
 @soysource =  Menu.create(name: 'しょうゆバター', price: '250', stock: '100')
 @plane = Menu.create(name: 'プレーン', price: '200', stock: '400')
 @curry = Menu.create(name: 'カレー', price: '250', stock: '30')
 
-Order.create(order_at: Time.zone.now, foods_attributes: [{menu_id: @soysource.id}])
-Order.create(order_at: Time.zone.now, foods_attributes: [{menu_id: @soysource.id}, {menu_id: @plane.id}])
+Order.create(
+  order_at: Time.zone.now,
+  foods_attributes: [{menu_id: @soysource.id, amount: 1, total: 250}])
+Order.create(
+  order_at: Time.zone.now,
+  foods_attributes:[
+    {menu_id: @soysource.id, amount: 1, total: 250},
+    {menu_id: @plane.id, amount: 2, total: 400}])
